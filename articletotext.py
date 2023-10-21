473abb3832254b69820d4228fc930477455c5243
Siavash Raissi <raissisiavash23@gmail.com>
2023-10-21 17:11:50 -0400

    added module

--- articletotext.py
+++ articletotext.py
@@ -1,13 +1,17 @@
 import newspaper
 
 # Step 3: Create a Article object using the newspaper module and pass the website link as a parameter
-article = newspaper.Article(url='https://www.cnn.com/2023/10/20/us/las-vegas-police-officer-casino-heists-cec/index.html')
 
-# Step 4: Download the article using the download() method of the Article object
-article.download()
+def convertArticleText(link):
+    article = newspaper.Article(url=link)
 
-# Step 5: Parse the article using the parse() method of the Article object
-article.parse()
+    # Step 4: Download the article using the download() method of the Article object
+    article.download()
 
-# Step 6: Extract the main article content using the text attribute of the Article object
-article_text = article.text
+    # Step 5: Parse the article using the parse() method of the Article object
+    article.parse()
+
+    # Step 6: Extract the main article content using the text attribute of the Article object
+    article_text = article.text
+
+    return(article.text)
