4a40dc734a5a6772bbc063973ec0adb1ec560a98
Siavash Raissi <raissisiavash23@gmail.com>
2023-10-21 18:55:38 -0400

    added dictionary parsing and GPT prompt setup

--- sentencegen.py
+++ sentencegen.py
@@ -4,28 +4,36 @@ import json
 openai.api_key = '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'
 
 
-def extract_features(emotions_file):
-    with open(emotions_file, 'r') as f:
-        emotions = json.load(f)
-    features = []
-    for emotion in emotions.values():
-        features.append(emotion)
-    print(emotions)
-    return features
+def extract_features(emotions_dict):
+    with open(emotions_dict) as f:
+        data = json.load(f)
+    score_message = 'The scores for the intensity of each emotion\'s level, separated by \';\' are: '
+    for i, emotion in enumerate(data):
+        score_message += f'{emotion}: {data[emotion]}'
+        if i == len(data) - 1:
+            score_message += '.'
+        else:
+            score_message += '; '
+    print(score_message)
     
 
-# def generate_sentence_with_emotion(original_sentence, emotion="happy"):
-#     prompt = f"Turn this sentence {original_sentence} into a {emotion} sentence:\n\nOriginal Sentence: {original_sentence}\nEmotion: {emotion}\nRewritten Sentence:"
+def generate_sentence_with_emotion(original_sentence, score_message):
+    prompt = f'''The following in quotes is a news article: \'{original_sentence}\'. 
+    
+    {score_message} 
 
-#     response = openai.Completion.create(
-#         engine="text-davinci-002",  # Choose the appropriate engine
-#         prompt=prompt,
-#         temperature=0.7,
-#         max_tokens=10
-#     )
+    Rewrite this article with a \'neutral\' score of .6.'
+    '''
 
-#     rewritten_sentence = response.choices[0].text.strip()
-#     return rewritten_sentence
+    response = openai.Completion.create(
+        engine="text-davinci-002",  # Choose the appropriate engine
+        prompt=prompt,
+        temperature=0.7,
+        max_tokens=10
+    )
+
+    rewritten_sentence = response.choices[0].text.strip()
+    return rewritten_sentence
 
 # Example usage:
 # original_sentence = "I love programming"
@@ -35,4 +43,4 @@ def extract_features(emotions_file):
 
 
 features1 = extract_features("C:/Users/raiss/Downloads/example_1.json")
-print(features1)
+# print(features1)
