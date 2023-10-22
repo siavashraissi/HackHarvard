5bcb1af95443a9a42fd5646a120cc299f372ebb0
Siavash Raissi <raissisiavash23@gmail.com>
2023-10-22 00:06:42 -0400

    flasking

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 from sentencegen import generate_sentence_with_emotion
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    new_article = generate_sentence_with_emotion()
-    return render_template('index.html', new_article=new_article)
+    # return("TEST")
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
